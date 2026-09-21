1420fd59899703637f5c0d4852e1cdfbe44878ca
Aspeternity <aspandre@163.com>
2026-09-22 02:02:06 +0800

    chore: version v0.8.0-alpha.2.4 and document Settings final polish

--- tests/UpdatePolicyTests.cpp
+++ tests/UpdatePolicyTests.cpp
@@ -143,6 +143,14 @@ int main() {
     assert(
         IsUpdateVersionNewer(
             "0.8.0-alpha.2.3",
+            "0.8.0-alpha.2.4"));
+    assert(
+        !IsUpdateVersionNewer(
+            "0.8.0-alpha.2.4",
+            "0.8.0-alpha.2.3"));
+    assert(
+        IsUpdateVersionNewer(
+            "0.8.0-alpha.2.4",
             "0.8.0-alpha.3"));
 
     assert(
@@ -177,6 +185,10 @@ int main() {
     assert(
         DefaultUpdateChannelForVersion(
             "0.8.0-alpha.2.3") ==
+        UpdateChannel::Development);
+    assert(
+        DefaultUpdateChannelForVersion(
+            "0.8.0-alpha.2.4") ==
         UpdateChannel::Development);
 
     assert(
