f99d2ca9fb0e3cca034ac2c1d87763632750cb69
Aspeternity <aspandre@163.com>
2026-09-26 14:35:24 +0800

    chore: guard Win32 compatibility macros

--- src/core/StartMenuProvider.cpp
+++ src/core/StartMenuProvider.cpp
@@ -7,8 +7,12 @@
 #include "../platform/LaunchTargetInspector.hpp"
 #include "../platform/WinUtil.hpp"
 
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <windows.h>
 #include <shlobj.h>
 
